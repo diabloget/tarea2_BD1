f3e7d0f7edd37513f9448b2364fb9c13ac343105
diabloget <diabloget@live.com>
2026-04-25 23:38:31 -0600

    Fix: Adaptacion del sp_carga para utilizar el XML completo aprobado por el profesor

--- sql/stored_procedures/sp_carga_xml.sql
+++ sql/stored_procedures/sp_carga_xml.sql
@@ -20,7 +20,7 @@ BEGIN
       x.value('@SalarioxHora', 'MONEY')
     FROM @xml.nodes('/Datos/Puestos/Puesto') AS T(x);
 
-    -- TiposMovimiento
+    -- TiposMovimiento 
     INSERT INTO dbo.TipoMovimiento (Id, Nombre, TipoAccion)
     SELECT
       x.value('@Id',         'INT'),
@@ -28,33 +28,29 @@ BEGIN
       x.value('@TipoAccion', 'VARCHAR(10)')
     FROM @xml.nodes('/Datos/TiposMovimientos/TipoMovimiento') AS T(x);
 
-    -- TiposEvento
+    -- TiposEvento 
     INSERT INTO dbo.TipoEvento (Id, Nombre)
     SELECT
       x.value('@Id',     'INT'),
       x.value('@Nombre', 'VARCHAR(128)')
     FROM @xml.nodes('/Datos/TiposEvento/TipoEvento') AS T(x);
 
-    -- Usuarios (UsuarioScripts tiene Id duplicado en el XML, se fuerza a 0)
-    INSERT INTO dbo.Usuario (Id, Username, Password)
-    VALUES (0, 'UsuarioScripts', 'UsuarioScripts');
-
+    -- Usuarios
     INSERT INTO dbo.Usuario (Id, Username, Password)
     SELECT
       x.value('@Id',     'INT'),
       x.value('@Nombre', 'VARCHAR(64)'),
       x.value('@Pass',   'VARCHAR(128)')
-    FROM @xml.nodes('/Datos/Usuarios/usuario') AS T(x)
-    WHERE x.value('@Nombre', 'VARCHAR(64)') <> 'UsuarioScripts';
+    FROM @xml.nodes('/Datos/Usuarios/usuario') AS T(x);
 
-    -- Errores
+    -- Errores 
     INSERT INTO dbo.Error (Codigo, Descripcion)
     SELECT
       x.value('@Codigo',      'INT'),
       x.value('@Descripcion', 'VARCHAR(256)')
-    FROM @xml.nodes('/Datos/Error/error') AS T(x);
+    FROM @xml.nodes('/Datos/e/error') AS T(x);
 
-    -- Empleados (Puesto viene como nombre -> lookup)
+    -- Empleados 
     INSERT INTO dbo.Empleado (IdPuesto, ValorDocumentoIdentidad, Nombre, FechaContratacion, SaldoVacaciones, EsActivo)
     SELECT
       (SELECT Id FROM dbo.Puesto WHERE Nombre = x.value('@Puesto', 'VARCHAR(128)')),
@@ -64,15 +60,19 @@ BEGIN
       0, 1
     FROM @xml.nodes('/Datos/Empleados/empleado') AS T(x);
 
-    -- Movimientos con NuevoSaldo acumulado por empleado
+    -- Movimientos con NuevoSaldo acumulado por empleado en orden cronológico
     ;WITH Parseados AS (
       SELECT
-        (SELECT Id FROM dbo.Empleado       WHERE ValorDocumentoIdentidad = x.value('@ValorDocId',       'VARCHAR(20)'))  AS IdEmpleado,
-        (SELECT Id FROM dbo.TipoMovimiento WHERE Nombre                  = x.value('@IdTipoMovimiento', 'VARCHAR(128)')) AS IdTipoMovimiento,
-        (SELECT TipoAccion FROM dbo.TipoMovimiento WHERE Nombre          = x.value('@IdTipoMovimiento', 'VARCHAR(128)')) AS TipoAccion,
+        (SELECT Id FROM dbo.Empleado
+          WHERE ValorDocumentoIdentidad = x.value('@ValorDocId', 'VARCHAR(20)'))        AS IdEmpleado,
+        (SELECT Id FROM dbo.TipoMovimiento
+          WHERE Nombre = x.value('@IdTipoMovimiento', 'VARCHAR(128)'))                   AS IdTipoMovimiento,
+        (SELECT TipoAccion FROM dbo.TipoMovimiento
+          WHERE Nombre = x.value('@IdTipoMovimiento', 'VARCHAR(128)'))                   AS TipoAccion,
         x.value('@Fecha',    'DATE')          AS Fecha,
         x.value('@Monto',    'DECIMAL(10,2)') AS Monto,
-        (SELECT Id FROM dbo.Usuario WHERE Username = x.value('@PostByUser', 'VARCHAR(64)'))                              AS IdPostByUser,
+        (SELECT Id FROM dbo.Usuario
+          WHERE Username = x.value('@PostByUser', 'VARCHAR(64)'))                        AS IdPostByUser,
         x.value('@PostInIP', 'VARCHAR(45)')   AS PostInIP,
         x.value('@PostTime', 'DATETIME')      AS PostTime
       FROM @xml.nodes('/Datos/Movimientos/movimiento') AS T(x)
@@ -93,7 +93,7 @@ BEGIN
     SELECT IdEmpleado, IdTipoMovimiento, Fecha, Monto, NuevoSaldo, IdPostByUser, PostInIP, PostTime
     FROM ConSaldo;
 
-    -- Actualizar SaldoVacaciones con el último movimiento de cada empleado
+    -- Actualizar SaldoVacaciones con el último movimiento
     UPDATE e
     SET e.SaldoVacaciones = (
       SELECT TOP 1 m.NuevoSaldo
